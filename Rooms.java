eac075ab0bd7561c0ab20424642d5b4c8764bb30
Lauren Pittman <lpittman@mail.umw.edu>
2019-11-12 19:35:47 +0000

    Here is the room class.

---
+++ Rooms.java
@@ -0,0 +1,16 @@
+class Rooms{
+	public String[][] rooms;
+	public int rows;
+	public int columns;
+	public Enemy enemy;
+	Battles battle = new Battles();
+
+	Rooms(){
+		this.rows = rows;
+		this.columns = columns;
+		rooms = new String[rows][columns];
+	}
+	public void fillRooms(Enemy enemy1, Item item1, Item item2, Item item3){
+
+
+	}
