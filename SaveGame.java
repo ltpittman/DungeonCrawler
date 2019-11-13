48558c1953c14738c2eff27c2daf917458cec120
Lauren Pittman <lpittman@mail.umw.edu>
2019-11-13 19:45:18 +0000

    The start of the SaveGame class.

--- SaveGame.java
+++ SaveGame.java
@@ -1,5 +1,25 @@
-class SaveGame{
+/*This class is used to load and save a game.*/
 
+class SaveGame{
+	//Instance Varibles.
+	Player player, playerName, playerHealth;
+	Inventory inventory;
+
+	Item items; //Or Item item1, Item item 2, etc.
+	Item getEquippedArmor, getEquippedWeapon;
+	
+	Enemy enemy, enemyName, enemyHealth;
+
+	World playerPosition, enemyPosition;
+
+	World world;
+	Rooms rooms;
+
+	//Method to save the game.
+	void persist(PrintWriter pw){
+
+	}
+	
 
 
 
