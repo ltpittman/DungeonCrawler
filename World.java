bcc71072234e6a56503a3b6776b91c30d2a0bfe5
Lauren Pittman <lpittman@mail.umw.edu>
2019-11-17 19:42:11 +0000

    World updated.

--- World.java
+++ World.java
@@ -166,27 +166,18 @@ class World{
 			if(input.equals("Y")){
 				System.out.println("You push the heavy rocks to the side and fit through...");
 				room2.fillRoom2(player, item1, item2);
-		}
+			}
 		}
 
 		System.out.println(Arrays.deepToString(world).replace(",","").replace("[[", "").replace("]]", "").replace("[","").replace("]",""));
-	
-	/*
-	   public void printWorld(){
-	   for(int i = 0; i < rows; i++){
-	   for(int j = 0; j < columns; j++){
-	   System.out.println(world[i][j]);
-	   }
-	   System.out.println();
-	   }
-	   }*/
-}
-//Method to print the commands to the user.
-void printCommands(){
-	System.out.println("[How to Play]:\nPress 'H' to go up.\nPress 'L' to go down.\nPress 'J' to go left.\nPress 'K' to go right.\nPress 'I' to print your inventory.\nPress 'D' to drop an item from your inventory.\nPress 'W' to equip a weapon from your inventory.\nPress 'A' to equip armor from your inventory.\nPress 'P' to print the commands to read again.");
-}
-//Method to print the objective of the game.
-void printObjective(){
-	System.out.println("\n[Welcome to Dungeon Crawler!]...\n   The overall objective of the game is to fight all the monsters that you encounter and make it out of the dungeon alive. To begin, your health will start at 100/100. You will have two items in your inventory: a sword and a piece of armor. While out searching for monsters, you have the option to pick up random items, whether it is a piece of armor, a weapon, etc.\n    You may switch your weapon and armor for something different that you find anytime. When it comes to fighting the monsters, it's all about luck. If you win a battle against a monster, you will be able to continue exploring the area and fight any more monsters you come across. If you are able to kill all the monsters in the dungeon, then you have won the game! [Good luck!]"); 
-}
+
+	}
+	//Method to print the commands to the user.
+	void printCommands(){
+		System.out.println("[How to Play]:\nPress 'H' to go up.\nPress 'L' to go down.\nPress 'J' to go left.\nPress 'K' to go right.\nPress 'I' to print your inventory.\nPress 'D' to drop an item from your inventory.\nPress 'W' to equip a weapon from your inventory.\nPress 'A' to equip armor from your inventory.\nPress 'P' to print the commands to read again.");
+	}
+	//Method to print the objective of the game.
+	void printObjective(){
+		System.out.println("\n[Welcome to Dungeon Crawler!]...\n   The overall objective of the game is to fight all the monsters that you encounter and make it out of the dungeon alive. To begin, your health will start at 100/100. You will have two items in your inventory: a sword and a piece of armor. While out searching for monsters, you have the option to pick up random items, whether it is a piece of armor, a weapon, etc.\n    You may switch your weapon and armor for something different that you find anytime. When it comes to fighting the monsters, it's all about luck. If you win a battle against a monster, you will be able to continue exploring the area and fight any more monsters you come across. If you are able to kill all the monsters in the dungeon, then you have won the game! [Good luck!]"); 
+	}
 }
