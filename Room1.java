b0128b171d6035b1ccdfe77ba64f1d443093d713
Lauren Pittman <lpittman@mail.umw.edu>
2019-11-20 19:49:58 +0000

    Made the two side rooms bigger.

--- Room1.java
+++ Room1.java
@@ -1,3 +1,6 @@
+/*class Room1: takes in the files for Room1.txt and stores it into an array. This particular room
+ * will be feautured as the main room when the game starts.*/ 
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -5,28 +8,28 @@ import java.util.Arrays;
 
 class Room1{
 	//Instance variables.
- 	public char[][] room;
+	public char[][] room;
 	public int rows;
 	public int columns;
 
 	//The constructor.
 	public Room1(String filename){
-		
+
 		this.rows = rows;
 		this.columns = columns;
 		room = new char[rows][columns];
 
 		try{
-		FileInputStream room1 = new FileInputStream("Room1.txt");
-		Scanner in = new Scanner(room1);
-		
-		for(int i = 0; i < rows; i++){
-			String l = in.nextLine();
-			for(int j = 0; i < l.length(); j++){
-				room[i][j] = l.charAt(j);
+			FileInputStream room1 = new FileInputStream("Room1.txt");
+			Scanner in = new Scanner(room1);
+
+			for(int i = 0; i < rows; i++){
+				String l = in.nextLine();
+				for(int j = 0; i < l.length(); j++){
+					room[i][j] = l.charAt(j);
+				}
 			}
-		}
-		in.close();
+			in.close();
 		}
 		catch(FileNotFoundException e){
 			System.out.println("File not found.");
