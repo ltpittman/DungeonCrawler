48753843063fa7cc5319ca2091f190fe16d93184
Lauren Pittman <lpittman@mail.umw.edu>
2019-12-05 15:23:56 +0000

    ...

--- Main.java
+++ Main.java
@@ -104,10 +104,7 @@ class Main{
 					key1.restore(a);
 					noth = a.nextLine();
 					key2.restore(a);
-<<<<<<< HEAD
 					noth = a.nextLine();
-=======
->>>>>>> a662574f1ddf210ad07aab6898cbfeef878a0e31
 					player.inventory.restore(a);
 					world.restore(a);
 					a.close();
